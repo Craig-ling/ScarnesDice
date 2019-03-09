f32376926f18ed2553f4c51d870a98fc3fcefd5a
Craig-ling <37049024+Craig-ling@users.noreply.github.com>
2019-03-08 23:26:46 -0500

    Added Handler object to increase the time for computer turn method call. Removed while loop for comp turn, replaced with recursion of handler and runnable objects/methods. Added Boolean class variable to help in ending the recursion in computer turn. Updated various comments. Fixed disabled buttons under computer turn. Fixed text to be displayed when a 1 is rolled (added another TexTView object)

--- app/src/main/java/com/example/lenovo/scarnesdice/MainActivity.java
+++ app/src/main/java/com/example/lenovo/scarnesdice/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lenovo.scarnesdice;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,12 +10,36 @@ import android.widget.TextView;
 
 import java.util.Random;
 
+/* The class variables keep track of the various scores determined by dice rolls.
+ * There is a Boolean variable that will assist in ending the computer turn.
+ * The handler object is used to delay calling the method that runs the computer turn,
+ * it takes the runnable object as an argument.*/
 public class MainActivity extends AppCompatActivity {
 
+    private static Boolean compTurnCheck = false;
     private static int userOverScore = 0;
     private static int userTurnScore = 0;
     private static int compOverScore = 0;
     private static int compTurnScore = 0;
+    private Handler handler = new Handler();
+
+    private Runnable runnable = new Runnable() {
+
+        @Override
+        public void run() {
+
+            // Returns and ends recursive call when computer turn ends.
+            if(compTurnCheck) {
+                alterButtons(true);
+                return;
+            }
+
+            computerTurn();
+
+            handler.postDelayed(this, 1000);
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +48,16 @@ public class MainActivity extends AppCompatActivity {
 
         final ImageView image = (ImageView) findViewById(R.id.imageView);
         final TextView text = (TextView) findViewById(R.id.textView);
+        final TextView text2 = (TextView) findViewById(R.id.textView2);
 
-        // Roll button is defined, on user click will call changeDie method to generate pseudo-random number between
-        // 1 and 6 inclusive. On a roll of 1, the user turn ends and copmuter turn is initiated.
+        // Roll button is defined, on user click will call changeDie method to generate pseudo-random
+        // number between 1 and 6 inclusive. On a roll of 1, the user turn ends and computer turn
+        // is initiated.
         final Button rollButton = (Button) findViewById(R.id.roll);
         rollButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
+                compTurnCheck = false;
                 int die = rollDie();
 
                 changeDie(die, image);
@@ -37,7 +65,9 @@ public class MainActivity extends AppCompatActivity {
                 if(die == 1) {
                     userTurnScore = 0;
                     setTextScore(text);
-                    computerTurn();
+                    setRollOneText(text2, "You rolled a 1!");
+                    handler.postDelayed(runnable, 1000);
+
                 }
                 else {
                     userTurnScore += die;
@@ -58,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
                 compTurnScore = 0;
 
                 setTextScore(text);
+                setRollOneText(text2, "");
             }
         });
 
@@ -69,8 +100,10 @@ public class MainActivity extends AppCompatActivity {
                 userTurnScore = 0;
 
                 setTextScore(text);
+                setRollOneText(text2, "");
 
-                computerTurn();
+                // Runs computer turn function with 1000 ms time delay.
+                handler.postDelayed(runnable, 1000);
             }
         });
 
@@ -94,13 +127,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // Generates and returns a pseudo-randon number between 1 and 6.
+    // Generates and returns a pseudo-random number between 1 and 6.
     public static int rollDie() {
         Random dieRoll = new Random();
 
-        int dieResult = dieRoll.nextInt(6) + 1;
-
-        return dieResult;
+        return dieRoll.nextInt(6) + 1;
     }
 
     // Modifies TextView object with score of user.
@@ -117,13 +148,19 @@ public class MainActivity extends AppCompatActivity {
         msg.setText(scoreText);
     }
 
-    // Ends the computer turn and sets appropriate text indicating a 1 has rolled.
+    // Ends the computer turn and sets appropriate text.
     public void endComputerTurn(TextView msg) {
         compTurnScore = 0;
 
         String scoreText = "Your score: "+userOverScore+" Computer score: "+compOverScore+" " +
-                "Computer turn score: "+compTurnScore+" Computer rolled a 1!";
+                "Computer turn score: "+compTurnScore;
         msg.setText(scoreText);
+    }
+
+    // Indicates to the user when a 1 was rolled.
+    public void setRollOneText(TextView msg, String s) {
+
+        msg.setText(s);
     }
 
   // Removes access to roll and hold buttons. Method to be used during computer turn.
@@ -136,38 +173,37 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    // Sets up a turn where the computer rolls a maximum score. Computer generates die rolls until their turn score
-    // is 20 or higher, or a 1 is rolled. Player buttons are disabled during the computer turn with the alterButton method.
+    // Sets up a turn where the computer rolls a maximum score. Computer generates die rolls until
+    // their turn score is 20 or higher, or a 1 is rolled. Player buttons are disabled during the
+    // computer turn with the alterButton method.
     public void computerTurn() {
         alterButtons(false);
 
         TextView text = (TextView) findViewById(R.id.textView);
         ImageView image = (ImageView) findViewById(R.id.imageView);
+        TextView text2 = (TextView) findViewById(R.id.textView2);
 
-        int cDie = 0;
+        int cDie = rollDie();
 
-        while(compTurnScore < 20) {
+        changeDie(cDie, image);
 
-            cDie = rollDie();
+        compTurnScore += cDie;
 
-            changeDie(cDie, image);
+            if(cDie == 1) {
+                compTurnCheck = true;
+                endComputerTurn(text);
+                setRollOneText(text2, "The computer rolled a 1!");
+
+            }
+            else if (compTurnScore >= 20) {
+                compTurnCheck = true;
+                compOverScore += compTurnScore;
+                compTurnScore = 0;
+                setRollOneText(text2, "The computer holds.");
+
+            }
 
             setCompScore(text);
 
-            if(cDie == 1) {
-                endComputerTurn(text);
-                compTurnScore = 0;
-                break;
-            }
-
-            compTurnScore += cDie;
-
-        }
-
-        compOverScore += compTurnScore;
-
-        setTextScore(text);
-
-        alterButtons(true);
     }
 }
